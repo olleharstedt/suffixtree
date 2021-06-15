5174ed459ce65c5612cb1431560b88c23c01c74d
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2021-06-15 10:12:37 +0200

    Make totokens eat a folder from CLI

--- totokens.php
+++ totokens.php
@@ -1,37 +1,50 @@
 <?php
 
-$file = "TokenDynamic.php";
-//$file = "em_manager_helper.php";
-$content = file_get_contents($file);
-$tokens = token_get_all($content);
+$file = $argv[1];
 $json = [];
 
-// Copied from phpcpd
-$tokensIgnoreList = [
-    T_INLINE_HTML        => true,
-    T_COMMENT            => true,
-    T_DOC_COMMENT        => true,
-    T_OPEN_TAG           => true,
-    T_OPEN_TAG_WITH_ECHO => true,
-    T_CLOSE_TAG          => true,
-    T_WHITESPACE         => true,
-    T_USE                => true,
-    T_NS_SEPARATOR       => true,
-];
-foreach($tokens as $token) {
-    if (is_array($token)) {
-        if (isset($tokensIgnoreList[$token[0]])) {
-            continue;
+$rdi = new RecursiveDirectoryIterator($file);
+foreach (new RecursiveIteratorIterator($rdi) as $file => $info) {
+    if ($file === '.' || $file === '..') {
+    } else {
+        $parts = pathinfo($file);
+        if (isset($parts['extension']) && $parts['extension'] === 'php') {
+            if (strpos($file, 'tests')) {
+                continue;
+            }
+            $content = file_get_contents($file);
+            $tokens = token_get_all($content);
+
+            // Copied from phpcpd
+            $tokensIgnoreList = [
+                T_INLINE_HTML        => true,
+                T_COMMENT            => true,
+                T_DOC_COMMENT        => true,
+                T_OPEN_TAG           => true,
+                T_OPEN_TAG_WITH_ECHO => true,
+                T_CLOSE_TAG          => true,
+                T_WHITESPACE         => true,
+                T_USE                => true,
+                T_NS_SEPARATOR       => true,
+            ];
+            foreach($tokens as $token) {
+                if (is_array($token)) {
+                    if (isset($tokensIgnoreList[$token[0]])) {
+                        continue;
+                    }
+                    $json[] = [
+                        'token_code' => $token[0],
+                        'token_name' => token_name($token[0]),
+                        'line' => $token[2],
+                        'content' => $token[1],
+                        'file' => $file
+                    ];
+                } 
+            }
         }
-        $json[] = [
-            'token_code' => $token[0],
-            'token_name' => token_name($token[0]),
-            'line' => $token[2],
-            'content' => $token[1],
-            'file' => $file
-        ];
-    } 
+    }
 }
+
 echo json_encode($json);
 
 //echo "\n";
